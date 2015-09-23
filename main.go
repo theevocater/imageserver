e9e3472c142c43aa1afd32447c9673886261a5b4
Jake Kaufman <jakeman@foursquare.com>
2015-09-23 01:12:11 -0700

    Add mux instead of hand parsing urls
    Using mux makes it *much* easier to parse urls. Eventually I'll add some fancier
    things. I also threw in "force" to help test query param things and also later
    when I am iterating on images.

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"regexp"
 	"strconv"
 	"strings"
 	"syscall"
 	"time"
 	"unsafe"
+
+	"github.com/gorilla/mux"
 )
 
 /*
@@ -22,10 +23,6 @@ import (
 #include <stdlib.h>
 */
 import "C"
-
-// /img/user/256x256/10029684-G5FUMQAZ10SCHGWV.jpg
-// /img/collection/dimensions/name?flags
-var handler_regxp = regexp.MustCompile("/img/([^/]+)/([^/]+)/([^/]+)")
 
 type Dimensions struct {
 	width, height int
@@ -51,38 +48,48 @@ func ParseDimensions(str string) Dimensions {
 	panic("Couldn't parse dimensions")
 }
 
-func ParseURL(url string) (string, string, string) {
-	matches := handler_regxp.FindStringSubmatch(url)
-	if len(matches) != 4 {
-		panic("Couldn't match url")
+type Request struct {
+	collection, dimensions, name string
+	force                        bool
+}
+
+func ParseRequest(vars map[string]string, query map[string][]string) Request {
+	collection := vars["collection"]
+	dimensions := vars["dimensions"]
+	name := vars["name"]
+	// TODO(jake) this is weird, maybe there is a better way
+	var force bool
+	if arr := query["force"]; len(arr) > 0 {
+		f, err := strconv.ParseBool(arr[0])
+		force = f
+		if err != nil {
+			force = false
+		}
 	}
-	collection := matches[1]
-	dimensions := matches[2]
-	name := matches[3]
-	return collection, dimensions, name
+
+	return Request{collection, dimensions, name, force}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	defer Write400(w)
 
-	bucket, dimensions_str, name := ParseURL(r.URL.Path)
-	file_struct := NewDiskImage(Conf.file_prefix, bucket, dimensions_str, name)
-	dimensions := ParseDimensions(dimensions_str)
+	request := ParseRequest(mux.Vars(r), r.URL.Query())
+	file_struct := NewDiskImage(Conf.file_prefix, request.collection, request.dimensions, request.name)
+	dimensions := ParseDimensions(request.dimensions)
 	fetched_file, resize := file_struct.read()
 
 	var resized_bytes []byte
 	var length int
 
-	if resize {
+	if resize || request.force {
 		log.Print("resizing")
 		length = len(fetched_file)
 		blob := C.resize_image(unsafe.Pointer(&fetched_file[0]), (*C.size_t)(unsafe.Pointer(&length)), (C.int)(dimensions.width), (C.int)(dimensions.height), 0, 13, 1.0)
 		defer C.free(blob)
 
-		// copy to go
+		// copy to go; I can make this faster with some "internal" things, but that can come later
 		resized_bytes = C.GoBytes(blob, (C.int)(length))
 
-		// is this safe?? I don't want to make too many copies
 		go file_struct.write(resized_bytes)
 	} else {
 		resized_bytes = fetched_file
@@ -120,7 +127,10 @@ func main() {
 		os.Exit(1)
 	}()
 
-	http.HandleFunc("/img/", handler)
+	r := mux.NewRouter()
+
+	// /img/collection/dimensions/name?flags
+	r.HandleFunc("/img/{collection}/{dimensions}/{name}", handler)
 	log.Print("Starting imageservice")
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", Conf.port), nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", Conf.port), r))
 }
