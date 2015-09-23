b92b4cb308878d96e3443e7b42445000c46b1941
Jake Kaufman <jakeman@foursquare.com>
2015-09-22 18:55:57 -0700

    Minor fixups
    run goimports
    actually use port

--- image_file.go
+++ image_file.go
@@ -1,66 +1,66 @@
 package main
 
 import (
-  "io/ioutil"
-  "log"
-  "os"
-  "path"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
 )
 
 type image_file interface {
-  read() ([]byte, bool)
-  write([]byte)
+	read() ([]byte, bool)
+	write([]byte)
 }
 
 type disk_image struct {
-  // full filename on disk
-  resized_name  string
-  original_name string
+	// full filename on disk
+	resized_name  string
+	original_name string
 }
 
 func NewDiskImage(prefix, bucket, dimensions, filename string) *disk_image {
-  img := new(disk_image)
-  img.original_name = path.Clean(path.Join(prefix, bucket, filename))
-  img.resized_name = path.Clean(path.Join(prefix, bucket, dimensions, filename))
-  return img
+	img := new(disk_image)
+	img.original_name = path.Clean(path.Join(prefix, bucket, filename))
+	img.resized_name = path.Clean(path.Join(prefix, bucket, dimensions, filename))
+	return img
 }
 
 func (image *disk_image) read() ([]byte, bool) {
-  var file, err = ioutil.ReadFile(image.resized_name)
-  var resize = false
+	var file, err = ioutil.ReadFile(image.resized_name)
+	var resize = false
 
-  if err != nil {
-    file, err = ioutil.ReadFile(image.original_name)
-    resize = true
-    log.Printf("found original file %s", image.original_name)
-    if err != nil {
-      panic(err)
-    }
-  } else {
-    log.Printf("found resized file %s", image.resized_name)
-  }
-  return file, resize
+	if err != nil {
+		file, err = ioutil.ReadFile(image.original_name)
+		resize = true
+		log.Printf("found original file %s", image.original_name)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		log.Printf("found resized file %s", image.resized_name)
+	}
+	return file, resize
 }
 
 func (image *disk_image) write(resized_image []byte) {
-  err := os.MkdirAll(path.Dir(image.resized_name), 0755)
-  err = ioutil.WriteFile(image.resized_name, resized_image, 0644)
-  if err != nil {
-    log.Print(err)
-  }
+	err := os.MkdirAll(path.Dir(image.resized_name), 0755)
+	err = ioutil.WriteFile(image.resized_name, resized_image, 0644)
+	if err != nil {
+		log.Print(err)
+	}
 }
 
 type s3_image struct {
-  filename string
-  bucket   string
-  //other stuff???
+	filename string
+	bucket   string
+	//other stuff???
 }
 
 func (image s3_image) read() ([]byte, bool) {
-  //todo
-  panic("Haven't implemented yet")
+	//todo
+	panic("Haven't implemented yet")
 }
 
 func (image s3_image) write(resized_image []byte) {
-  panic("Haven't implemented yet")
+	panic("Haven't implemented yet")
 }
--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "os"
-  "os/signal"
-  "regexp"
-  "strconv"
-  "strings"
-  "syscall"
-  "time"
-  "unsafe"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"regexp"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+	"unsafe"
 )
 
 /*
@@ -27,94 +28,96 @@ import "C"
 var handler_regxp = regexp.MustCompile("/img/([^/]+)/([^/]+)/([^/]+)")
 
 type Dimensions struct {
-  width, height int
+	width, height int
 }
 
 func Write400(w http.ResponseWriter) {
-  if r := recover(); r != nil {
-    log.Print(r)
-    w.WriteHeader(http.StatusBadRequest)
-  }
+	if r := recover(); r != nil {
+		log.Print(r)
+		w.WriteHeader(http.StatusBadRequest)
+	}
 }
 
 func ParseDimensions(str string) Dimensions {
-  split := strings.Split(str, "x")
-  if len(split) == 2 {
-    width, err := strconv.Atoi(split[0])
-    if err != nil {
-      panic("Couldn't parse dimensions")
-    }
-    height, err := strconv.Atoi(split[1])
-    return Dimensions{width, height}
-  }
-  panic("Couldn't parse dimensions")
+	split := strings.Split(str, "x")
+	if len(split) == 2 {
+		width, err := strconv.Atoi(split[0])
+		if err != nil {
+			panic("Couldn't parse dimensions")
+		}
+		height, err := strconv.Atoi(split[1])
+		return Dimensions{width, height}
+	}
+	panic("Couldn't parse dimensions")
 }
 
 func ParseURL(url string) (string, string, string) {
-  matches := handler_regxp.FindStringSubmatch(url)
-  if len(matches) != 4 {
-    panic("Couldn't match url")
-  }
-  collection := matches[1]
-  dimensions := matches[2]
-  name := matches[3]
-  return collection, dimensions, name
+	matches := handler_regxp.FindStringSubmatch(url)
+	if len(matches) != 4 {
+		panic("Couldn't match url")
+	}
+	collection := matches[1]
+	dimensions := matches[2]
+	name := matches[3]
+	return collection, dimensions, name
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  defer Write400(w)
+	defer Write400(w)
 
-  bucket, dimensions_str, name := ParseURL(r.URL.Path)
-  file_struct := NewDiskImage(Conf.file_prefix, bucket, dimensions_str, name)
-  dimensions := ParseDimensions(dimensions_str)
-  fetched_file, resize := file_struct.read()
+	bucket, dimensions_str, name := ParseURL(r.URL.Path)
+	file_struct := NewDiskImage(Conf.file_prefix, bucket, dimensions_str, name)
+	dimensions := ParseDimensions(dimensions_str)
+	fetched_file, resize := file_struct.read()
 
-  var resized_bytes []byte
-  var length int
+	var resized_bytes []byte
+	var length int
 
-  if resize {
-    log.Print("resizing")
-    length = len(fetched_file)
-    blob := C.resize_image(unsafe.Pointer(&fetched_file[0]), (*C.size_t)(unsafe.Pointer(&length)), (C.int)(dimensions.width), (C.int)(dimensions.height), 0, 13, 1.0)
-    defer C.free(blob)
+	if resize {
+		log.Print("resizing")
+		length = len(fetched_file)
+		blob := C.resize_image(unsafe.Pointer(&fetched_file[0]), (*C.size_t)(unsafe.Pointer(&length)), (C.int)(dimensions.width), (C.int)(dimensions.height), 0, 13, 1.0)
+		defer C.free(blob)
 
-    // copy to go
-    resized_bytes = C.GoBytes(blob, (C.int)(length))
+		// copy to go
+		resized_bytes = C.GoBytes(blob, (C.int)(length))
 
-    // is this safe?? I don't want to make too many copies
-    go file_struct.write(resized_bytes)
-  } else {
-    resized_bytes = fetched_file
-    length = len(fetched_file)
-  }
+		// is this safe?? I don't want to make too many copies
+		go file_struct.write(resized_bytes)
+	} else {
+		resized_bytes = fetched_file
+		length = len(fetched_file)
+	}
 
-  w.Header().Add("Content-Type", "image/png")
-  w.Header().Add("Content-Length", strconv.Itoa(length))
-  w.Header().Add("Last-Modified", time.Now().Format(time.RFC1123))
-  w.WriteHeader(http.StatusOK)
-  w.Write(resized_bytes)
+	w.Header().Add("Content-Type", "image/png")
+	w.Header().Add("Content-Length", strconv.Itoa(length))
+	w.Header().Add("Last-Modified", time.Now().Format(time.RFC1123))
+	w.WriteHeader(http.StatusOK)
+	w.Write(resized_bytes)
 }
 
 var Conf Confs = Confs{"images/", 8000}
 
 type Confs struct {
-  file_prefix string
-  port        int
+	file_prefix string
+	port        int
 }
 
 func main() {
-  C.CreateMagick()
-  defer C.DestroyMagick()
-  signal_chan := make(chan os.Signal, 1)
-  signal.Notify(signal_chan, os.Interrupt)
-  signal.Notify(signal_chan, syscall.SIGTERM)
-  signal.Notify(signal_chan, syscall.SIGINT)
-  go func() {
-    <-signal_chan
-    C.DestroyMagick()
-    os.Exit(1)
-  }()
-  http.HandleFunc("/img/", handler)
-  log.Print("Starting imageservice")
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	C.CreateMagick()
+	defer C.DestroyMagick()
+	// something is going on here with graphics magick causing it to crash w/
+	// term :(
+	signal_chan := make(chan os.Signal, 1)
+	signal.Notify(signal_chan, os.Interrupt)
+	signal.Notify(signal_chan, syscall.SIGTERM)
+	signal.Notify(signal_chan, syscall.SIGINT)
+	go func() {
+		<-signal_chan
+		C.DestroyMagick()
+		os.Exit(1)
+	}()
+	http.HandleFunc("/img/", handler)
+	log.Print("Starting imageservice")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", Conf.port), nil))
 }
